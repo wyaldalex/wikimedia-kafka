db07616a1f46e0b63dbce1684cd16f42c893ea09
AlexAR <moltron_nano@hotmail.com>
2022-08-24 09:56:01 -0600

    feat: initial setup of multi model kafka demo app with 1 module

---
+++ kafka-producer-wikimedia/src/main/java/com/tudux/SpringBootProducerApplication.java
@@ -0,0 +1,15 @@
+package com.tudux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//Modules are being created with Maven's quick start archetype
+@SpringBootApplication
+public class SpringBootProducerApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SpringBootProducerApplication.class);
+    }
+}
---
+++ src/test/java/com/tudux/wikimediakafka/WikimediaKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tudux.wikimediakafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
